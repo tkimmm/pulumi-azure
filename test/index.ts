f475c3d604ba56231a449f7d49212fa7ab5340de
Tom Kim <tkimm@me.com>
2021-06-22 12:50:33 +0200

    added example files

---
+++ test/index.ts
@@ -0,0 +1,15 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+
+// Create an Azure Resource Group
+const resourceGroup = new resources.ResourceGroup("rgtest");
+
+// Create an Azure resource (Storage Account)
+const storageAccount = new storage.StorageAccount("satest", {
+    resourceGroupName: resourceGroup.name,
+    sku: {
+        name: storage.SkuName.Standard_LRS,
+    },
+    kind: storage.Kind.StorageV2,
+});
